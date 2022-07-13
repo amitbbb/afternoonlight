df32026a4aa958f9739d7560f76a1a69a9eb8e95
amitbbb <htmedia.amitkumar@gmail.com>
2022-07-13 19:14:05 +0530

    first commit

---
+++ hydrogen.config.js
@@ -0,0 +1,18 @@
+import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    defaultCountryCode: 'US',
+    defaultLanguageCode: 'EN',
+    storeDomain: 'bbb-hydrogen-test.myshopify.com',
+    storefrontToken: '7cc2b02b3239d686571bb890b5682a0f',
+    storefrontApiVersion: '2022-07',
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: import.meta.env.PROD,
+    sameSite: 'Strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+});
